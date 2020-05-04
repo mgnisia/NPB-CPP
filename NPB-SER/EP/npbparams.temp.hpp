9d9a764eafe315356bcdab199f1107c5e8094349
Moritz Gnisia <moritzgnisia@gmail.com>
2020-05-04 12:24:13 +0200

    aded template for header file

---
+++ NPB-SER/EP/npbparams.temp.hpp
@@ -0,0 +1,18 @@
+/* CLASS = A */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class_npb of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#cmakedefine CLASS "@CLASS@"
+#cmakedefine M "@M@"
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "04 May 2020"
+#define NPBVERSION "4.0"
+#define CS1 "g++ -std=c++14"
+#define CS2 "$(CC)"
+#define CS3 "-lm "
+#define CS4 "-I../common "
+#define CS5 "-O3"
+#define CS6 "-O3"
+#define CS7 "randdp"
