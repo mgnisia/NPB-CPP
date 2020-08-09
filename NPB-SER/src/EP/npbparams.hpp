58ebc5da46cf8f7bbb4e6e884a2964e71ce58fb7
Moritz Gnisia <moritzgnisia@gmail.com>
2020-08-09 08:45:35 +0200

    update cmake build system for seriale approach

---
+++ NPB-SER/src/EP/npbparams.hpp
@@ -0,0 +1,9 @@
+/* CLASS = S */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class_npb of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#define	CLASS	 'S'
+#define	M	24
+#define	CONVERTDOUBLE	FALSE
